368e2c0f48d8ef854cc535c55d2b415668c5bab4
Felipe Merchan <daniel.merchan@gradiweb.com>
2021-07-17 22:51:02 -0500

    feat: add feature in order to forward and rewind

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const $video = document.querySelector('#video')
 const $play = document.querySelector('#play')
 const $pause = document.querySelector('#pause')
+const $backward = document.querySelector('#backward')
+const $forward = document.querySelector('#forward')
 
 function handlePlay() {
   $video.play()
@@ -14,5 +16,15 @@ function handlePause() {
   $pause.hidden = true
 }
 
+function handleBackward() {
+  $video.currentTime -= 10
+}
+
+function handleForward() {
+  $video.currentTime += 10
+}
+
 $play.addEventListener('click', handlePlay)
 $pause.addEventListener('click', handlePause)
+$backward.addEventListener('click', handleBackward)
+$forward.addEventListener('click', handleForward)
